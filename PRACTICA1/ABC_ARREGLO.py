c2ead15affd8113daf3384c35363e69ff7a874be
Andrea_Avila <aavilamartinez120@gmail.com>
2026-02-11 13:42:26 -0600

    CAMBIO

---
+++ PRACTICA1/ABC_ARREGLO.py
@@ -0,0 +1,15 @@
+cadena = "Parangaricutirimicuaro"
+
+abecedario_min = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ = [letra.upper() for letra in abecedario_min]
+
+print("Letras minúsculas:")
+for letra in abecedario_min:
+    if cadena.find(letra) != -1:
+        print (letra, ":", cadena.count(letra))
+
+print("\nLetras mayúsculas:")
+for letra in abecedario_may:
+    if cadena.find(letra) != -1:
+        print (letra, ":", cadena.count(letra))
+        
